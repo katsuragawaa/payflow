c678215be82843d142a99e8d5ee7c306b23fa32f
Katsuragawa <andre.katsuragawa@gmail.com>
2021-11-04 22:13:32 -0300

    lock device orientation

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import './modules/login/login_page.dart';
-import './modules/splash/splash_page.dart';
-import './shared/themes/app_colors.dart';
-import './modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+import 'shared/themes/app_colors.dart';
+import 'modules/home/home_page.dart';
+import 'modules/barcode_scanner/barcode_scanner_page.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +28,7 @@ class AppWidget extends StatelessWidget {
         '/splash': (context) => SplashPage(),
         '/login': (context) => LoginPage(),
         '/home': (context) => HomePage(),
+        '/barcode_scanner': (context) => BarcodeScannerPage(),
       },
     );
   }
